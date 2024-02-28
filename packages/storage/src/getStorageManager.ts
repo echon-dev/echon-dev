c9754778a7511f24e928966b0bf344badf88e6d0
brian-nunez <bnunez9999@gmail.com>
2024-02-27 22:56:03 -0700

    feat(storage): add local manager

--- packages/storage/src/getStorageManager.ts
+++ packages/storage/src/getStorageManager.ts
@@ -1,12 +1,16 @@
+import { LocalManager } from "./LocalManager";
 import { S3Manager } from "./S3Manager";
 export type StorageType = 'local' | 's3';
 
 export function getStorageManager(type?: StorageType) {
   switch(type) {
+    case 'local':
+      return LocalManager;
+
     case 's3':
       return S3Manager;
 
     default:
-      throw new Error('[Error]: storage type must be provided');
+      throw new Error('[Error]: storage type must be either "local" or "s3"');
   }
 }
