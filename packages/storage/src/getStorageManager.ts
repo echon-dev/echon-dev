c2de47970e631687461f15ff583afd4864f4b750
brian-nunez <bnunez9999@gmail.com>
2024-03-14 19:19:11 -0700

    feat(storage): add cloudflare and linode

--- packages/storage/src/getStorageManager.ts
+++ packages/storage/src/getStorageManager.ts
@@ -1,13 +1,19 @@
 import { LocalManager } from "./LocalManager";
 import { S3Manager } from "./S3Manager";
-export type StorageType = 'local' | 's3';
+export type StorageType = 'local' | 's3' | 'cloudflare' | 'linode';
 
 export function getStorageManager(type?: StorageType) {
-  switch(type) {
+  switch (type) {
     case 'local':
       return LocalManager;
 
     case 's3':
+      return S3Manager;
+
+    case 'cloudflare':
+      return S3Manager;
+
+    case 'linode':
       return S3Manager;
 
     default:
